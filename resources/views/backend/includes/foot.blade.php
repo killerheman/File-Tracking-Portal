8b55d9fd4dfe8b43afa6d8cac6983b4ffe2f3415
KillerHeman <sharmahcool5@gmail.com>
2022-09-16 11:26:19 +0530

    first

---
+++ resources/views/backend/includes/foot.blade.php
@@ -0,0 +1,16 @@
+ <!-- BEGIN: Vendor JS-->
+ <script src="{{asset('backend/app-assets/vendors/js/vendors.min.js')}}"></script>
+ <script src="{{asset('backend/app-assets/vendors/js/forms/select/select2.full.min.js')}}"></script>
+ <!-- BEGIN Vendor JS-->
+
+ <!-- BEGIN: Page Vendor JS-->
+ <script src="{{asset('backend/app-assets/vendors/js/ui/prism.min.js')}}"></script>
+ <!-- END: Page Vendor JS-->
+
+ <!-- BEGIN: Theme JS-->
+ <script src="{{asset('backend/app-assets/js/core/app-menu.js')}}"></script>
+ <script src="{{asset('backend/app-assets/js/core/app.js')}}"></script>
+ <!-- END: Theme JS-->
+
+ <!-- BEGIN: Page JS-->
+ <!-- END: Page JS-->
---
+++ resources/views/backend/includes/layout.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+
+<html class="loading" lang="en" data-textdirection="ltr">
+<!-- BEGIN: Head-->
+
+<head>
+
+    @include('backend.includes.head')
+
+</head>
+<!-- END: Head-->
+
+<!-- BEGIN: Body-->
+
+<body class="vertical-layout vertical-menu-modern 2-columns  navbar-floating footer-static   menu-collapsed"
+    data-open="click" data-menu="vertical-menu-modern" data-col="2-columns">
+
+    <!-- BEGIN: Header-->
+    @include('backend.includes.topbar')
+    <!-- END: Header-->
+
+
+    <!-- BEGIN: Main Menu-->
+    @include('backend.includes.sidebar')
+    <!-- END: Main Menu-->
+
+    <!-- BEGIN: Content-->
+    <div class="app-content content">
+        <div class="content-overlay"></div>
+        <div class="header-navbar-shadow"></div>
+        <div class="content-wrapper">
+            @yield('content')
+        </div>
+    </div>
+    <!-- END: Content-->
+
+    <div class="sidenav-overlay"></div>
+    <div class="drag-target"></div>
+
+
+    @include('backend.includes.footer')
+
+
+    @include('backend.includes.foot')
+
+</body>
+<!-- END: Body-->
+
+</html>
